a8748733220d2e6073e75d133f1e60724986f15d
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-01 13:00:19 -0400

    Create all_guesses array.

--- guessing_game.rb
+++ guessing_game.rb
@@ -6,8 +6,23 @@
 #If you guess the same number twice, the program should ask you if you're feeling all right (or something similarly sarcastic).
 #Your code should include at least two methods.
 
-puts "You have 5 chances to guess a number between 1 and 100. Enter a number:"
-guess = gets.chomp
+correct_number = rand(1...100)
+puts correct_number
 
-rand(1..100)
-#should generate a number between 1 and 100 
+all_guesses = Array.new
+
+5.times do
+  puts "You have 5 chances to guess a number between 1 and 100. Enter a number:"
+  guess = gets.chomp
+  all_guesses << guess
+  puts all_guesses
+  if guess > correct_number
+    puts "You guessed too high! Try again."
+  elsif
+    guess < correct_number
+    puts "You guessed too low! Try again."
+  else
+    puts "You guessed the correct number!"
+    break
+  end
+end
