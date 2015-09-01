10d6f222d7016880dd163d76f469dd96cbc5321b
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-01 16:07:13 -0400

    Add logic for multiple too high or too low guesses.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,7 +1,9 @@
 # Normal mode
 
-correct_number = (1..100).to_a.sample
-# rand(1..100)
+# You might say "50" and then be told "that's too low." If you then guess "25,"
+# you're just wasting a guess.
+
+correct_number = (1..100).to_a.sample  # rand(1..100)
  puts correct_number
 
 all_guesses = Array.new
@@ -33,16 +35,23 @@ until count == 5
   if already_guessed?(all_guesses, guess)
     puts "You already guessed this number, you numbskull!"
   else
+		all_guesses.each do |value|
+			if value.to_i > guess.to_i && value.to_i < correct_number
+					puts "You guessed too low AGAIN! You feelin ok?"
+			elsif value.to_i < guess.to_i && value.to_i > correct_number
+					puts "You guessed too high AGAIN! You feelin ok?"
+			end
+		end
     all_guesses << guess
-    if is_numeric?(guess)
-      guess = guess.to_i
-      count += 1
-      break if guess_check(guess, correct_number)
-    else
-      puts "That is not a number! Please enter a number."
-    end
-    if count == 5 && guess.to_i != correct_number
-    	puts "You have guessed incorrectly 5 times. GAME OVER!"
-    end
+				if is_numeric?(guess)
+      		guess = guess.to_i
+      		count += 1
+      		break if guess_check(guess, correct_number)
+    		else
+      		puts "That is not a number! Please enter a number."
+    		end
+	end
+  if count == 5 && guess.to_i != correct_number
+    puts "You have guessed incorrectly 5 times. GAME OVER!"
   end
 end
