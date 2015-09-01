f8b026695f5328842ca2ebecc62a7890bd60b2f9
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-01 13:22:00 -0400

    added decremental counter for each turn

--- guessing_game.rb
+++ guessing_game.rb
@@ -7,22 +7,31 @@
 #Your code should include at least two methods.
 
 correct_number = rand(1...100)
-# puts correct_number
+ puts correct_number
 
 all_guesses = Array.new
 
-5.times do
-  puts "You have 5 chances to guess a number between 1 and 100. Enter a number:"
+counter = 0
+while counter < 5
+  counter += 1
+#5.times do
+  puts "You have #{6 - counter} chances to guess a number between 1 and 100. Enter a number:"
   guess = gets.chomp
   all_guesses << guess
-# puts all_guesses
-  if guess > correct_number
-    puts "You guessed too high! Try again."
-  elsif
-    guess < correct_number
-    puts "You guessed too low! Try again."
-  else
-    puts "You guessed the correct number!"
-    break
+  case
+    when guess.to_i >= correct_number
+      puts "You guessed too high! Try again."
+    when guess.to_i <= correct_number
+      puts "You guessed too low! Try again."
+    when all_guesses.include?(guess)
+      puts "You already guessed that, you numbskull!"
+  when guess.to_i == correct_number
+      puts "You guessed the correct number!"
+      break
   end
 end
+
+puts all_guesses
+
+#Things to fix
+# 1) "Try again" shouldn't appear on the 5th try
